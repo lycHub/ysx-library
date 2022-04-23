f39655fecc80d36a396d0a3feb7bde644f1b145a
lycHub <1120805364@qq.com>
2022-04-23 20:40:55 +0800

    ci

--- projects/demo/src/App.vue
+++ projects/demo/src/App.vue
@@ -1,68 +1,70 @@
 <template>
-  App
-  <a-button>Add</a-button>
-  <vir-tree
-    :source="list"
-    :show-checkbox="showCheckbox"
-    :check-strictly="checkStrictly"
-    :render-node="renderNode"
-    :render-icon="renderIcon"
-    :default-disabled-keys="defaultDisabledKeys"
-    :default-selected-key="defaultSelectedKey"
-    :default-expanded-keys="defaultExpandedKeys"
-    :default-checked-keys="defaultCheckedKeys">
-    <template #node="node">
-      <b style="color: green;"><i>{{ node.name }}</i></b>
-    </template>
-    <template #icon="{ loading, expanded }">
-      <span>ico-{{ expanded }}</span>
-    </template>
-  </vir-tree>
+  <a-layout class="app-layout">
+    <a-layout-header class="header">
+      <h2 class="title">Vue virtual tree</h2>
+      <a class="link" href="//github.com/lycHub/vue-virtual-tree" target="_blank">github</a>
+    </a-layout-header>
+    <a-layout-content class="content">
+      <doc-container />
+      <a-anchor class="anchor">
+        <a-anchor-link href="#usage" title="安装使用" />
+        <a-anchor-link href="#base-demo" title="基本用法" />
+        <a-anchor-link href="#checkbox-demo" title="可勾选" />
+        <a-anchor-link href="#async-dada-demo" title="异步加载" />
+        <a-anchor-link href="#custom-node-demo" title="自定义渲染节点" />
+        <a-anchor-link href="#custom-icon-demo" title="自定义图标" />
+        <a-anchor-link href="#api" title="API" />
+      </a-anchor>
+    </a-layout-content>
+    <a-layout-footer class="footer">
+      Ant Design ©2018 Created by Ant UED
+    </a-layout-footer>
+  </a-layout>
 </template>
-
-<script setup lang="tsx">
-  import { onMounted } from 'vue';
-  import { TreeNodeOptions, VirTree, BaseTreeNode } from '@ysx/vue-virtual-tree';
-
-  function recursion(path = '0', level = 3, h = 6): TreeNodeOptions[] {
-      const list = [];
-      for (let i = 0; i < h; i += 1) {
-        const nodeKey = `${path}-${i}`;
-        const treeNode: TreeNodeOptions  = {
-          nodeKey,
-          name: nodeKey,
-          children: []
-        };
-
-        if (level > 0) {
-          treeNode.children = recursion(nodeKey, level - 1);
-        }
-        list.push(treeNode);
-      }
-      return list;
-    }
-
-    const showCheckbox = false;
-    const checkStrictly = false;
-    let list = $ref(recursion());
-    let defaultDisabledKeys = $ref(['0-3-1']);
-    let defaultSelectedKey = $ref('');
-    let defaultExpandedKeys = $ref(['0-1-1']);
-    let defaultCheckedKeys = $ref<string[]>(['0-1-0', '0-1-1', '0-1-2', '0-1-3', '0-1-4',]);
-    setTimeout(() => {
-      // defaultExpandedKeys = [];
-      // defaultCheckedKeys = ['0-2'];
-      // list = recursion('0', 3, 4);
-    }, 2000)
-    onMounted(() => {
-      
-    });
-
-
-    function renderNode(node: BaseTreeNode) {
-      return <div style="padding: 0 4px;"><b style="color: #f60;">{ node.name }</b></div>
-    }
-    function renderIcon({ expanded }: any) {
-      return <div>i-{ expanded.toString() }</div>;
-    }
+<script lang="tsx">
+  import { defineComponent, ref } from 'vue';
+  import DocContainer from './doc/index.vue';
+  export default defineComponent({
+    components: { DocContainer },
+    setup() {
+      return {
+        selectedKeys: ref<string[]>(['2'])
+      };
+    },
+  });
 </script>
+<style scoped>
+  .app-layout {
+    --top-bottom-height: 64px;
+  }
+
+    .app-layout .header {
+      display: flex;
+      align-items: center;
+      color: var(--white-color);
+      height: var(--top-bottom-height);
+    }
+    
+    .app-layout .header .title {
+      color: inherit;
+      margin: 0;
+    }
+    .app-layout .header .link {
+      margin-left: 20px;
+      font-size: var(--font-size-mid);
+    }
+    .app-layout  .content {
+      position: relative;
+      min-height: calc(100vh - var(--top-bottom-height) * 2);
+      background-color: var(--white-color);
+      
+    }
+    .app-layout .content .anchor {
+      position: absolute;
+      right: 50px;
+      top: 30px;
+    }
+    .app-layout .footer {
+      text-align: center;
+    }
+</style>
