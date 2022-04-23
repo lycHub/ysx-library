e71cc8acbe1a411d165e68ef04fa9923261cb9fd
lycHub <1120805364@qq.com>
2022-04-24 03:59:11 +0800

    baseUrl

--- projects/demo/vite.config.ts
+++ projects/demo/vite.config.ts
@@ -4,6 +4,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '//vue-tree-4gi26kh195ddd24f-1257117519.tcloudbaseapp.com/',
   plugins: [
     vue({
       reactivityTransform: true
