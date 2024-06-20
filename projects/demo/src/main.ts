2aeb5c19d39b4cb4aa06d0187502aececcf8f9d9
ysx <1120805364@qq.com>
2024-06-20 18:00:41 +0800

    fix demo

--- projects/demo/src/main.ts
+++ projects/demo/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import GlobalComponents from './components'
+import GlobalComponents from './components';
 import './styles/index.css';
-
+console.log('object :>> ');
 createApp(App).use(GlobalComponents).mount('#app');
